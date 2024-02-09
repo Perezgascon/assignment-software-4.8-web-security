dfd3f367c0850bee90540426e5140a83189ae5d4
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2024-02-09 16:10:14 +0700

    created server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { testConnection } = require('./db/conn');
+
+const app = express();
+
+const PORT = 8080;
+
+
+testConnection();
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+})
+
